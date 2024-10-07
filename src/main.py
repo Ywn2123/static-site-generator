2b68b7efb278ac2ed1d9d71bb209b96da21845e2
Ywn2123 <muzkurnis@gmail.com>
2024-10-07 14:35:08 +0300

    updated textnode and main

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    a = TextNode('fuck you', 'italic', 'http://....')
+    b = TextNode('fuck you', 'italic', 'http://....')
+    c = TextNode('hello', 'bold', '')
+
+if __name__ == "__main__":
+    main()
